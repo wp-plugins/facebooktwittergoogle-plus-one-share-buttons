4e6549195c65c310d9a4ed4ab2ad1c28a56fcc53
Jeriff Cheng <Jeriff Cheng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-09 17:48:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebooktwittergoogle-plus-one-share-buttons/tags/1.2.2@516888 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Facebook_Twitter_Google_plus-one_Share_Buttons.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: Facebook,Twitter,Google plus one Share Buttons
+Plugin URI: http://www.92app.com/wordpress-plugins/facebook-twitter-google-plus-one-share-buttons
+Description: This simple and easy-to-use plugin adds facebook,twitter,google +1(plus one) and many other popular social sharing buttons after post contents,such as:Stumbleupon,Linkedin,Digg
+Version: 1.2.2
+Author: Jeriff Cheng
+Author URI: http://www.92app.com/
+*/
+
+/*
+Copyright 2011  Jeriff Cheng(Email: hschengyongtao@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+*/
+//load the javascript
+function socialbutton_integration() {
+if (is_single()) { ?> 
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+<script src="http://platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo get_option('siteurl') . '/' . PLUGINDIR . '/' . dirname(plugin_basename (__FILE__))?>/FB.Share" ></script>
+<script type="text/javascript">
+(function() {
+var s = document.createElement('SCRIPT'), s1 = document.getElementsByTagName('SCRIPT')[0];
+s.type = 'text/javascript';
+s.async = true;
+s.src = 'http://widgets.digg.com/buttons.js';
+s1.parentNode.insertBefore(s, s1);
+})();
+</script>
+
+<?php }
+ }
+add_action('wp_footer', 'socialbutton_integration');
+
+//insert the buttons after post contents
+function socialbutton($content) {
+if(is_single()) {
+$content.= '
+<style>
+*{margin:0; padding:0;}
+#socialbuttonnav li{background:none;overflow:hidden;width:65px; height:80px; line-height:30px; margin-right:2px; float:left; text-align:center;}
+#fb { text-align:center;border:none; }
+</style>
+<ul id="socialbuttonnav" style="list-style:none;width:90%; overflow:hidden;margin:0 auto;">
+<li><!-- Facebook share--><a name="fb_share" type="box_count" href="http://www.facebook.com/sharer.php">Share</a></li>
+<li><!-- Twitter--><div><a name="twitter_share" data-count="vertical" href="http://twitter.com/share" class="twitter-share-button" >Tweet</a></div></li>
+<li><!-- Google plus one--><div><g:plusone size="tall" count="true"></g:plusone></div></li>
+<li><!-- stumbleupon--><div><script src="http://www.stumbleupon.com/hostedbadge.php?s=5"></script></div></li>
+<li><!-- linkedin--><div><script type="in/share" data-counter="top"></script></div></li>
+<li><!-- digg--><div><a class="DiggThisButton DiggMedium" href="http://digg.com/submit?url="<?php the_permalink();?>"&amp;title="<?php the_title(); ?>""></a></div></li>
+<li><!-- Facebook like--><div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like href="" send="false" layout="box_count" width="64" show_faces="false" action="like" font=""></fb:like></li>
+</ul>';
+}
+return $content;
+}
+add_filter ('the_content', 'socialbutton');
---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== Facebook,Twitter,Google plus one Share Buttons ===
+Contributors: Jeriff Cheng
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FUYPGH626DA34
+Tags: facebook,twitter,google +1(plus one) button,google plus one,facebook share,facebook like,twitter share,stumbleupon,linkedin,digg
+Requires at least: 2.8+
+Tested up to: 3.2.1
+Stable tag: trunk
+
+== Description ==
+This simple yet easy-to-use plugin adds facebook,twitter,google +1(plus one) and many other popular social sharing buttons after post contents,such as:Stumbleupon,Linkedin and Digg.
+
+<strong>Support me by introducing this plugin to your friends, or give me a high rate on WordPress.org.Thanks !</strong>
+
+<strong>Features:<strong>
+
+1. All buttons can show counts.
+
+2. The Javascript required is embedded in the footer of your webpage,thus, doesn't affect your PageSpeed(As pagespeed affects Google PageRank, this plugin is SEO-friendly).
+
+3. Only show share buttons after post contents by default.(See FAQs-2 for how to show buttons on Page or HomePage)
+
+For more information,refer to <a title="Facebook,Twitter,Google plus one Share Buttons by Jeriff Cheng" rel="follow" href="http://www.92app.com/wordpress-plugins/facebook-twitter-google-plus-one-share-buttons" target="_blank">Facebook,Twitter,Google plus one Share Buttons Doc </a>
+
+My personal blog:<a rel="follow" href="http://www.92app.com" target="_blank">www.92app.com</a>
+
+== Installation ==
+1.  Upload the full directory into your wp-content/plugins directory
+2.  Activate the plugin at the plugin administration page
+3.  That's it!
+
+== Frequently Asked Questions ==
+For FAQs and Bug report,refer to <a rel="follow" title="Facebook,Twitter,Google plus one Share Buttons by Jeriff Cheng" href="http://www.92app.com/wordpress-plugins/facebook-twitter-google-plus-one-share-buttons">Facebook,Twitter,Google plus one Share Buttons Doc</a>
+
+== Screenshots ==
+1. Facebook,Twitter,Google plus one and many other popular social sharing buttons after post contents
+
+
+== Changelog ==
+
+=V1.0 -16.07.2011=
+
+*First Version
+
+=V1.1 -26.07.2011=
+ <ul>
+	<li>NEW : Add Facebook like button</li>
+	<li>Changed : Change the horizontal style to vertical</li>
+	<li>Bugfix : Facebook Share Button doesn't  always display count(fixed)</li>
+</ul>
+
+=V1.2 -27.07.2011=
+ <ul>
+	<li>NEW : Add Stumbleupon,Linkedin,Digg,Google Buzz share buttons</li>
+	<li>Changed : Remove the Facebook share buttons(Facebook will soon give up supporting the share buttons, they recommend the Like button)</li>
+</ul>
+
+=V1.2.1 -28.07.2011=
+
+*Fix Facebook like button compatibility issues with Chrome and Safari
+
+== Upgrade Notice ==
+
+=1.1=
+This version fix a bug and add Facebook like button,recommend upgrading.
+
+=1.2=
+Add Stumbleupon,Linkedin,Digg,Google Buzz share buttons.Remove the facebook share buttons(Facebook will soon give up supporting the share buttons, they recommend the Like button)
+
+=1.2.1=
+Fix Facebook like button compatibility issues with Chrome and Safari
+
+=1.2.2=
+Remove buzz button,google has stopped buzz service
+Add facebook share button again.
